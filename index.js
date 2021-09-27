588973a273e21637bf640b112839ed38ad157259
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2021-09-27 13:33:43 +0500

    Working web app

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded());
+
+app.set('view engine', 'ejs');
+
+//global variable
+let postList = [];
+
+
+app.listen(port, () => console.log(`Example app listening on port port!`));
+
+//Home Route
+
+app.get('/', function (req, res) {
+    res.render('home', { postList: postList });
+});
+
+//Compose Route
+
+app.get('/compose', function (req, res) {
+    res.render('compose', {});
+});
+
+app.post('/compose', function (req, res) {
+    const title = req.body.compose_title;
+    const post = req.body.compose_post;
+
+    postList.push({ title: title, post: post });
+
+    //console.log(postList);
+
+    res.redirect('/');
+
+});
